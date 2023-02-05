40ede1e9e61dd3265ff4abd0ca2d2faf08f824e6
jeaivr <snezzsp@gmail.com>
2023-02-05 20:57:43 +0100

    update

--- src/components/moviecard.jsx
+++ src/components/moviecard.jsx
@@ -15,29 +15,8 @@ export function MovieCard({ movie }) {
   const imageUrl = GetMovieImg(movie.poster_path, 300);
 
   return (
-    <div  className={styles.moviegrid} id={styles.articles}>
-      {/* <Link to={"/movies/" + movie.id}>
-        <div className={styles.movieinfo}>
-          <p className={styles.movierelease}>
-            {movie.release_date.substr(0, 4)}
-          </p>
-          <br></br>
-          <p className={styles.movietitle}>{movie.title}</p>
-          <p className={styles.moviegenres}>
-            {movie.genre_ids.map((genre) => replace(genre)).join(",")}
-          </p>
-        </div>
-        <img
-          width={300}
-          height={450}
-          className={styles.movieimage}
-          src={imageUrl}
-          alt={movie.title}
-        />
-      </Link> */}
-
+    <div id={styles.articles}>
       <article>
-        <div>
         <img
           width={300}
           height={450}
@@ -45,9 +24,10 @@ export function MovieCard({ movie }) {
           src={imageUrl}
           alt={movie.title}
         />
-        </div>
         <div className={styles.moreInfo}>
-          <span className={styles.release}>{movie.release_date.substr(0, 4)}</span>
+          <span className={styles.release}>
+            {movie.release_date.substr(0, 4)}
+          </span>
           <b className={styles.title}>{movie.title}</b>
           <span>
             <b>{movie.genre_ids.map((genre) => replace(genre)).join(", ")}</b>
