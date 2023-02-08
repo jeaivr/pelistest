9e819f371648c0dc2bd837447a344b6c0c05b98a
jeaivr <snezzsp@gmail.com>
2023-02-08 14:01:45 +0100

    update

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,21 +1,35 @@
 import styles from "./sidebar.module.css";
-import { BiMoviePlay } from "react-icons/bi";
+import { BiMoviePlay, BiSearch } from "react-icons/bi";
+import { GoSettings } from "react-icons/go";
+import { FiSave, FiInfo, FiUser } from "react-icons/fi";
+import { BiSearchAlt } from "react-icons/bi";
+import { MdOutlineContactSupport } from "react-icons/md";
 
 export function SideMenu() {
   return (
     <div>
       <header>
-        <a className={styles.icon} active><BiMoviePlay /></a>
-		<nav>
-			<i className={styles.icon_filter} onclick="filter(this);"></i>
-			<i className={styles.icon_search} onclick="searchClick(this);"></i>
-			<i className={styles.icon_user} onclick="watchList(this);"></i>
-			<a className={styles.icon_version + ' ' + styles.newVers} up-target="body"></a>
-			<a className={styles.icon_userSet} up-target="body"></a>
-		</nav>
-		<b className={styles.alpha}>Alfa</b>
-		<p className={styles.version}>0.1.0</p>
-	</header>
+        <a className={styles.icon} active>
+          <BiMoviePlay />
+        </a>
+        <a className={styles.icon} active>
+          <GoSettings />
+        </a>
+        <a className={styles.icon}>
+          <BiSearchAlt />
+        </a>
+        <a className={styles.icon}>
+          <FiUser />
+        </a>
+        <a className={styles.icon + " " + styles.iconcontact}>
+          <MdOutlineContactSupport />
+        </a>
+        <a className={styles.icon + " " + styles.iconinfo}>
+			<FiInfo />
+			</a>
+        <b className={styles.alpha}>Alfa</b>
+        <p className={styles.version}>© 2017</p>
+      </header>
     </div>
   );
 }
